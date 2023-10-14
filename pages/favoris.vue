41b174681a95998859069b219bea81d5f1c3e377
Brice Vignal <vignal.brice@gmail.com>
2023-10-14 16:29:28 +0200

    feat(pages): add favorites, index and liste pages

---
+++ pages/favoris.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-7xl">Welcome to the favorite page</h1>
+  </div>
+</template>
---
+++ pages/liste.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-7xl">Welcome to the list page</h1>
+  </div>
+</template>
