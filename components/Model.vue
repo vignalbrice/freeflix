e2ddd9bfcb87f62bc9fa66b9f241741e8069b2c2
Brice Vignal <vignal.brice@gmail.com>
2023-10-19 09:38:45 +0200

    feat(remove - nuxt project): remove nuxt project initialized

--- components/Model.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { TresCanvas } from "@tresjs/core";
-import { OrbitControls, Box } from "@tresjs/cientos";
-</script>
-<template>
-  <TresCanvas shadows alpha></TresCanvas>
-</template>
--- pages/liste.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1 class="text-7xl">Welcome to the list page</h1>
-  </div>
-</template>
